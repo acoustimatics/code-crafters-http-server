93a332e9214f446badf4f2d0197b29ed51b24d29
Christopher Cowan <chrisjoelcowan@gmail.com>
2025-07-12 11:11:20 -0400

    Extrac URL path

--- src/Server.cs
+++ src/Server.cs
@@ -6,21 +6,48 @@ using var server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 Console.WriteLine("started");
 
-using var socket = server.AcceptSocket();
-Console.WriteLine("accepted socket");
-
-var buffer = new byte[1024];
-var bytesReceived = 0;
-do
+while (true)
 {
-    Console.WriteLine("receiving bytes");
-    bytesReceived = socket.Receive(buffer);
-    Console.WriteLine($"received {bytesReceived} bytes");
-}
-while (socket.Available > 0);
+    using var socket = server.AcceptSocket();
+    Console.WriteLine("[accepted socket]");
 
-Console.WriteLine("sending response");
-var response = Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
-socket.Send(response);
-Console.WriteLine("response sent");
-Console.WriteLine("exiting");
+    var buffer = new byte[1024];
+    var bytesReceived = 0;
+    var request = "";
+    do
+    {
+        Console.WriteLine("[receiving bytes]");
+        bytesReceived = socket.Receive(buffer);
+        Console.WriteLine($"[received {bytesReceived} bytes]");
+
+        request += Encoding.ASCII.GetString(buffer, index: 0, count: bytesReceived);
+    }
+    while (socket.Available > 0);
+
+    Console.WriteLine("[received request]");
+    Console.Write(request);
+
+    var requestLines = request.Split("\r\n");
+    var requestLine = requestLines[0];
+    var requestLineElements = requestLine.Split(' ');
+    var requestTarget = requestLineElements[1];
+    Console.WriteLine($"[message target] {requestTarget}");
+
+    string responseStatus;
+    if (requestTarget == "/")
+    {
+        responseStatus = "200 OK";
+    }
+    else
+    {
+        responseStatus = "400 Not Found";
+    }
+
+    var responseString = $"HTTP/1.1 {responseStatus}\r\n\r\n";
+    Console.WriteLine($"[response is] {responseString}");
+
+    Console.WriteLine("[sending response]");
+    var response = Encoding.ASCII.GetBytes(responseString);
+    socket.Send(response);
+    Console.WriteLine("[response sent]");
+}
