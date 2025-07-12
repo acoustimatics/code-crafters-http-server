bbc83208a5f0503b748ae361f646bf8e7ead0b29
Christopher Cowan <chrisjoelcowan@gmail.com>
2025-07-12 10:37:00 -0400

    Respond with 200

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,26 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
+using var server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+Console.WriteLine("started");
+
+using var socket = server.AcceptSocket();
+Console.WriteLine("accepted socket");
+
+var buffer = new byte[1024];
+var bytesReceived = 0;
+do
+{
+    Console.WriteLine("receiving bytes");
+    bytesReceived = socket.Receive(buffer);
+    Console.WriteLine($"received {bytesReceived} bytes");
+}
+while (socket.Available > 0);
+
+Console.WriteLine("sending response");
+var response = Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+socket.Send(response);
+Console.WriteLine("response sent");
+Console.WriteLine("exiting");
