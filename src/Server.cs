904622a06062ef9256d4e494473312421d28b6f4
Christopher Cowan <chrisjoelcowan@gmail.com>
2025-07-13 10:17:21 -0400

    Respond with body

--- src/Server.cs
+++ src/Server.cs
@@ -1,53 +1,100 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
+
+var requestHandlers = new Func<string, string?>[]
+{
+    RequestDefault,
+    RequestEcho,
+};
 
 using var server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-Console.WriteLine("started");
 
 while (true)
 {
     using var socket = server.AcceptSocket();
-    Console.WriteLine("[accepted socket]");
 
     var buffer = new byte[1024];
     var bytesReceived = 0;
     var request = "";
     do
     {
-        Console.WriteLine("[receiving bytes]");
         bytesReceived = socket.Receive(buffer);
-        Console.WriteLine($"[received {bytesReceived} bytes]");
-
         request += Encoding.ASCII.GetString(buffer, index: 0, count: bytesReceived);
     }
     while (socket.Available > 0);
-
-    Console.WriteLine("[received request]");
-    Console.Write(request);
 
     var requestLines = request.Split("\r\n");
     var requestLine = requestLines[0];
     var requestLineElements = requestLine.Split(' ');
     var requestTarget = requestLineElements[1];
-    Console.WriteLine($"[message target] {requestTarget}");
 
-    string responseStatus;
-    if (requestTarget == "/")
+    Console.WriteLine(requestLine);
+
+    string? response = null;
+    foreach (var requestHandler in requestHandlers)
     {
-        responseStatus = "200 OK";
+        response = requestHandler(requestTarget);
+        if (response != null)
+        {
+            Send(socket, response);
+            break;
+        }
     }
-    else
+
+    if (response == null)
     {
-        responseStatus = "404 Not Found";
+        Send(socket, $"HTTP/1.1 404 Not Found\r\n\r\n");
     }
-
-    var responseString = $"HTTP/1.1 {responseStatus}\r\n\r\n";
-    Console.WriteLine($"[response is] {responseString}");
-
-    Console.WriteLine("[sending response]");
-    var response = Encoding.ASCII.GetBytes(responseString);
-    socket.Send(response);
-    Console.WriteLine("[response sent]");
 }
+
+void Send(Socket socket, string response)
+{
+    var responseBytes = Encoding.ASCII.GetBytes(response);
+    socket.Send(responseBytes);
+}
+
+string? RequestDefault(string requestTarget)
+{
+    var regex = new Regex("^/$");
+    var match = regex.Match(requestTarget);
+    if (!match.Success)
+    {
+        return null;
+    }
+    return $"HTTP/1.1 200 OK\r\n\r\n";
+}
+
+string? RequestEcho(string requestTarget)
+{
+    var regex = new Regex("^/echo/(?<str>[^/]+)$");
+    var match = regex.Match(requestTarget);
+    if (!match.Success)
+    {
+        return null;
+    }
+
+    var str = match.Groups["str"]?.Value;
+    if (str == null)
+    {
+        return null;
+    }
+
+    var response = new StringBuilder();
+
+    response.Append("HTTP/1.1 200 OK\r\n");
+
+    response.Append("Content-Type: text/plain\r\n");
+
+    var contentLength = Encoding.ASCII.GetByteCount(str);
+    response.Append("Content-Length: ").Append(contentLength).Append("\r\n");
+
+    response.Append("\r\n");
+
+    response.Append(str);
+
+    return response.ToString();
+}
+
